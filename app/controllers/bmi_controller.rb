b9c20944eb6415f38ed046aa58116fc1ccd90000
wenjunjiang8021 <wenjunjiang8021@gmail.com>
2019-06-12 00:10:11 -0700

    page code

--- app/controllers/bmi_controller.rb
+++ app/controllers/bmi_controller.rb
@@ -1,13 +1,13 @@
 
-class BmiController < ApplicationController
-	def index
-	end
+  class BmiController < ApplicationController
+    def index
+    end
 
-	def result
-		height = params[:body_height].to_f / 100  #把單位換成公尺
-		weight = params[:body_weight].to_f
+    def result
+      height = params[:body_height].to_f / 100   # 把單位換算成公尺
+      weight = params[:body_weight].to_f
 
-		# bmi 計算公式：bmi = 體重 (單位：公斤) / 身高平方 (單位： 公尺)
-		@bmi = (weight / (height * height)).round(2)
-	end
- end
+      # BMI 計算公式: BMI = 體重（單位：公斤） / 身高平方（單位：公尺）.
+      @bmi = (weight / (height * height)).round(2)
+    end
+  end
